f4ee17f26f22f3ccdf5991a7a48400edce427eca
Eden Park <edenGoforit@gmail.com>
2021-05-31 21:39:44 +1200

    Housekeeping: remove ununecessary files

--- client/client.jsx
+++ client/client.jsx
@@ -1,5 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+import { App } from "./App";
+import { copyAnswersWithModifiedUpvotes } from "../shared/utility";
+
+let state = undefined;
+
+console.info("Client:: Fetching data from server");
+
+fetch("http://localhost:7777/data")
+  .then((data) => data.json())
+  .then((json) => {
+    state = json;
+    render();
+  });
+
+function handleVote(answerId, increment) {
+  state.answers = copyAnswersWithModifiedUpvotes(
+    state.answers,
+    answerId,
+    increment
+  );
+
+  fetch(`vote/${answerId}?increment=${increment}`);
+
+  render();
+}
+
+function render() {
+  console.info("Client:: Rendering application with remote data", state);
+  ReactDOM.hydrate(
+    <App {...state} handleVote={handleVote} />,
+    document.querySelector("#root")
+  );
+}

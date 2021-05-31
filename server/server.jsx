aae04b14fe988d85a1e2bdbb0aa1176509d8975d
Eden Park <edenGoforit@gmail.com>
2021-05-31 21:40:49 +1200

    Feat: for now eslit not enabled, formatted code for files in the server folder are pushed

--- server/server.jsx
+++ server/server.jsx
@@ -1,11 +1,11 @@
-import express from 'express';
-import React from 'react';
-import compression from 'compression';
-import { renderToString } from 'react-dom/server';
-import { readFileSync } from 'fs';
+import express from "express";
+import React from "react";
+import compression from "compression";
+import { renderToString } from "react-dom/server";
+import { readFileSync } from "fs";
 
-import { App } from '../client/App';
-import { getData, modifyAnswerUpvotes } from './database';
+import { App } from "../client/App";
+import { getData, modifyAnswerUpvotes } from "./database";
 
 const app = new express();
 const port = 7777;
@@ -14,28 +14,24 @@ app.use(compression());
 app.use(express.static("dist"));
 
 app.get("/data", async (_req, res) => {
-
-    res.json(await getData());
-
+  res.json(await getData());
 });
 
 app.get("/vote/:answerId", (req, res) => {
-
-    const { query, params } = req;
-    modifyAnswerUpvotes(params.answerId, parseInt(query.increment));
-
+  const { query, params } = req;
+  modifyAnswerUpvotes(params.answerId, parseInt(query.increment));
 });
 
-app.get("/", async ( _req, res )=>{
+app.get("/", async (_req, res) => {
+  const { questions, answers } = await getData();
 
-    const { questions, answers } = await getData();
+  const rendered = renderToString(
+    <App questions={questions} answers={answers} />
+  );
 
-    const rendered = renderToString(<App questions={questions} answers={answers}/>);
+  const index = readFileSync(`public/index.html`, `utf8`);
 
-    const index = readFileSync(`public/index.html`, `utf8`);
-
-    res.send(index.replace("{{rendered}}", rendered));
-    
+  res.send(index.replace("{{rendered}}", rendered));
 });
 
 app.listen(port);
